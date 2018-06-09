1daeb44b023287f467d1eadcf7bd3ca7c60daf26
vv <thehedgeify@gmail.com>
2018-06-09 12:06:13 +0200

    initial setup

---
+++ wikiwars/src/wikiwars/Wikiwars.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wikiwars;
+
+/**
+ *
+ * @author vv
+ */
+public class Wikiwars {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //input Thema
+        String topic = "Foreign_relations_of_North_Korea";
+        String url = "https://en.wikipedia.org/wiki/"+topic;
+        //get data per Wiki API
+        
+        //get Watson response
+        
+        //output to .csv
+        
+        //start R Script
+        
+    }
+    
+}
