044ac9e4f4874115d67e7344a8d25364af376156
Michael Gajda <michael.gajda@tu-dortmund.de>
2018-06-09 13:38:41 +0200

    Add basic Watson example

--- wikiwars/src/main/java/wikiwars/Wikiwars.java
+++ wikiwars/src/main/java/wikiwars/Wikiwars.java
@@ -5,8 +5,11 @@
  */
 package wikiwars;
 
+import com.ibm.watson.developer_cloud.http.ServiceCall;
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.NaturalLanguageUnderstanding;
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.*;
+
 /**
- *
  * @author vv
  */
 public class Wikiwars {
@@ -17,14 +20,47 @@ public class Wikiwars {
     public static void main(String[] args) {
         //input Thema
         String topic = "Foreign_relations_of_North_Korea";
-        String url = "https://en.wikipedia.org/wiki/"+topic;
+        String url = "https://en.wikipedia.org/wiki/" + topic;
         //get data per Wiki API
-        
+
         //get Watson response
-        
+        NaturalLanguageUnderstanding service = new NaturalLanguageUnderstanding(
+                "2018-03-16",
+                "56e32079-1ebb-41af-882e-ed8e90fdcb50",
+                "b1qmkh6h0sgt"
+        );
+        service.setEndPoint( "https://gateway-fra.watsonplatform.net/natural-language-understanding/api");
+
+        String text = "I love all baby kittens.";
+
+        EntitiesOptions entitiesOptions = new EntitiesOptions.Builder()
+                .sentiment(true)
+                .limit(2)
+                .build();
+
+        SentimentOptions sentimentOptions = new SentimentOptions.Builder()
+                .build();
+
+        Features features = new Features.Builder()
+                .entities(entitiesOptions)
+                .sentiment(sentimentOptions)
+                .build();
+
+        AnalyzeOptions parameters = new AnalyzeOptions.Builder()
+                .text(text)
+                .features(features)
+                .build();
+
+        AnalysisResults response = service
+                .analyze(parameters)
+                .execute();
+        System.out.println(response);
+
+
         //output to .csv
-        
+
         //start R Script
+
     }
-    
+
 }
